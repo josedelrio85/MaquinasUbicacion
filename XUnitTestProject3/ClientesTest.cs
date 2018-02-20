c69b363cb49692ffbf88605541d013dbe22165de
warnier <josedelrio@warnier.com>
2018-02-20 16:55:47 +0100

    Pruebas tipo XUnit

---
+++ XUnitTestProject3/ClientesTest.cs
@@ -0,0 +1,118 @@
+﻿using MaquinasUbicacion.Model;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XUnitTestProject3
+{
+    public class TestInitializer
+    {
+        private readonly HttpClient _client;
+        private readonly TestServer _server;
+
+        public TestInitializer()
+        {
+            _server = new TestServer(new WebHostBuilder()
+                .UseStartup<TestStartup>());
+
+            _client = _server.CreateClient();
+        }
+
+        [Fact]
+        public async Task Test1()
+        {
+            var response = await _client.GetAsync("/api/clientes");
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            //JArray aa = JArray.Parse(responseString);
+            System.Collections.Generic.List<Cliente> lista = JArray.Parse(responseString).ToObject<System.Collections.Generic.List<Cliente>>();
+
+            Assert.NotNull(responseString);
+            Assert.NotEmpty(lista);
+            Assert.Equal(4, lista.Count);
+        }
+
+        [Fact]
+        public async Task Test2()
+        {
+            var response = await _client.GetAsync("/api/clientes/1");
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            dynamic jsonObject = JObject.Parse(responseString);
+            Cliente cliente = JsonConvert.DeserializeObject<Cliente>(responseString);
+
+            Assert.NotNull(responseString);
+            Assert.IsType<Cliente>(cliente);
+            // Assert.Same(cliente, new Cliente { id = 1, nombre = "Cliente Test 1" });
+            //Assert.Equal(cliente, new Cliente { id = 1, nombre = "Cliente Test 1" });
+        }
+
+        [Fact]
+        public async Task Test3()
+        {
+            var response = await _client.GetAsync("/api/clientes/SearchClientes/1");
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            List<Cliente> lista = JArray.Parse(responseString).ToObject<List<Cliente>>();
+
+            Assert.NotNull(responseString);
+            Assert.NotEmpty(lista);
+            Assert.Single<Cliente>(lista);
+            Assert.Contains("Cliente Test 1", lista[0].nombre);
+        }
+
+        [Fact]
+        public async Task Test4()
+        {
+            Cliente cliente = new Cliente { id = 5, nombre = "Cliente Test de Prueba " };
+            var response = await _client.PostAsJsonAsync("/api/clientes/", cliente).ContinueWith((postTask) => postTask.Result.EnsureSuccessStatusCode());
+            
+            Assert.NotNull(response);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            var clienteResp = JsonConvert.DeserializeObject<Cliente>(responseString);
+            Assert.Equal(5, clienteResp.id);
+        }
+
+        [Fact]
+        public async Task Test5()
+        {
+            Cliente cliente = new Cliente { id = 4, nombre = "Cliente Test 6" };
+            var response = await _client.PutAsJsonAsync("/api/clientes/" + cliente.id, cliente).ContinueWith((putTask) => putTask.Result.EnsureSuccessStatusCode());
+
+            Assert.NotNull(response);
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            var clientePut = JsonConvert.DeserializeObject<Cliente>(responseString);
+
+            Assert.Equal(4, clientePut.id);
+            Assert.Contains("Test 6", cliente.nombre);
+
+        }
+
+
+        [Fact]
+        public async Task Test6()
+        {
+            var response = await _client.GetAsync("/api/clientes/1");
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            dynamic jsonObject = JObject.Parse(responseString);
+            Cliente cliente = JsonConvert.DeserializeObject<Cliente>(responseString);
+
+            Assert.NotNull(responseString);
+            Assert.IsType<Cliente>(cliente);
+        }
+    }
+}
---
+++ XUnitTestProject3/TestStartup.cs
@@ -0,0 +1,66 @@
+﻿using MaquinasUbicacion.Model;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+
+namespace XUnitTestProject3
+{
+    public class TestStartup
+    {
+        public TestStartup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+
+            services.AddDbContext<maqUbiContext>(options =>
+            options.UseInMemoryDatabase("prueba"));
+
+
+
+            services.AddMvc();
+
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        { 
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            var context = app.ApplicationServices.GetService<maqUbiContext>();
+            AddTestData(context);
+
+            app.UseMvc();
+        }
+
+        private static void AddTestData(maqUbiContext context)
+        {
+            context.Cliente.Add(new Cliente { id = 1, nombre = "Cliente Test 1" });
+            context.Cliente.Add(new Cliente { id = 2, nombre = "Cliente Test 2" });
+            context.Cliente.Add(new Cliente { id = 3, nombre = "Cliente Test 3" });
+            context.Cliente.Add(new Cliente { id = 4, nombre = "Cliente Test 4" });
+
+            context.Ubicacion.Add(new Ubicacion { id = 1, ubicacion = "Ubicacion Test 1", idCliente = 1 });
+            context.Ubicacion.Add(new Ubicacion { id = 2, ubicacion = "Ubicacion Test 2", idCliente = 2 });
+            context.Ubicacion.Add(new Ubicacion { id = 3, ubicacion = "Ubicacion Test 3", idCliente = 3 });
+            context.Ubicacion.Add(new Ubicacion { id = 4, ubicacion = "Ubicacion Test 4", idCliente = 4 });
+
+            context.Maquina.Add(new Maquina { id = 1, modelo = "Modelo 1", nSerie = "123456", tipo = "Tipo 1", idCliente = 1, idUbicacion = 1 });
+            context.Maquina.Add(new Maquina { id = 2, modelo = "Modelo 2", nSerie = "98745z", tipo = "Tipo 2", idCliente = 2, idUbicacion = 2 });
+            context.Maquina.Add(new Maquina { id = 3, modelo = "Modelo 3", nSerie = "79813w", tipo = "Tipo 3", idCliente = 3, idUbicacion = 3 });
+            context.Maquina.Add(new Maquina { id = 4, modelo = "Modelo 4", nSerie = "36985t", tipo = "Tipo 4", idCliente = 4, idUbicacion = 4 });
+
+            context.SaveChanges();
+        }
+    }
+}
